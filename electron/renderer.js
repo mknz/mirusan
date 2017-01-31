d3a0d168f6768f8b2754b9628db4c69b1c865077
mkanazu <mknz@users.noreply.github.com>
2017-02-01 01:04:51 +0900

    Read config from json file

--- electron/renderer.js
+++ electron/renderer.js
@@ -1,48 +1,54 @@
 'use strict';
-const path = require('path');
+(function () {
+  const path = require('path');
+  const fs = require('fs');
 
-// Adjust inline frame size with window
-function resizeFrame() {
-  const margin = 50; // Influenced by header's height
-  var windowHeight = document.documentElement.clientHeight;
-  var elem = document.getElementById('pdf-viewer-container');
-  elem.style.height = windowHeight - margin + 'px';
-  var elem = document.getElementById('sidebar-container');
-  elem.style.height = windowHeight - margin + 'px';
-}
+  // Read config from file
+  const Config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
-// init elem sizes
-// workaround: wait until container elements appear
-setTimeout(resizeFrame, 100);
-
-window.addEventListener('resize', function(e) {
-  resizeFrame()
-}, false);
-
-// Embed elm
-var app = Elm.Main.embed(document.getElementById('window'));
-
-// Open new pdf when openNewFile msg comes from elm
-app.ports.openNewFile.subscribe(function(resp) {
-  var pdfFilePath = resp[0];
-  var pageNum = resp[1];
-  document.getElementById('pdf-viewer').contentWindow.location.replace('./pdfjs/web/viewer.html?file=' + pdfFilePath + '&page=' + pageNum.toString());
-});
-
-// Get document paths to add DB by dialog, send them back to elm
-var elem = document.getElementById('getFilesToAddDB');
-elem.addEventListener('change', function(e) {
-  var files = e.target.files;
-  var filePaths = [];
-  for (var i = 0 ; i < files.length ; i++ )
-  {
-    filePaths.push(files[i].path);
+  // Adjust inline frame size with window
+  function resizeFrame() {
+    const margin = 50; // Influenced by header's height
+    var windowHeight = document.documentElement.clientHeight;
+    var elem = document.getElementById('pdf-viewer-container');
+    elem.style.height = windowHeight - margin + 'px';
+    var elem = document.getElementById('sidebar-container');
+    elem.style.height = windowHeight - margin + 'px';
   }
-  app.ports.filesToAddDB.send(filePaths);
-}, false);
 
-// Invoke file open dialog.
-app.ports.getFilesToAddDB.subscribe(function() {
+  // init elem sizes
+  // workaround: wait until container elements appear
+  setTimeout(resizeFrame, 100);
+
+  window.addEventListener('resize', function(e) {
+    resizeFrame()
+  }, false);
+
+  // Embed elm
+  var app = Elm.Main.embed(document.getElementById('window'));
+
+  // Open new pdf when openNewFile msg comes from elm
+  app.ports.openNewFile.subscribe(function(resp) {
+    var pdfFilePath = resp[0];
+    var pageNum = resp[1];
+    document.getElementById('pdf-viewer').contentWindow.location.replace('./pdfjs/web/viewer.html?file=' + pdfFilePath + '&page=' + pageNum.toString());
+  });
+
+  // Get document paths to add DB by dialog, send them back to elm
   var elem = document.getElementById('getFilesToAddDB');
-  elem.click();
-});
+  elem.addEventListener('change', function(e) {
+    var files = e.target.files;
+    var filePaths = [];
+    for (var i = 0 ; i < files.length ; i++ )
+    {
+      filePaths.push(files[i].path);
+    }
+    app.ports.filesToAddDB.send(filePaths);
+  }, false);
+
+  // Invoke file open dialog.
+  app.ports.getFilesToAddDB.subscribe(function() {
+    var elem = document.getElementById('getFilesToAddDB');
+    elem.click();
+  });
+})()
