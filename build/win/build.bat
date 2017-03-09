8b2a94360ca3b192d272d4b2330f4af73623adf8
mkanazu <mknz@users.noreply.github.com>
2017-03-10 00:30:41 +0900

    Fix binary bundle in build

--- build/win/build.bat
+++ build/win/build.bat
@@ -1,8 +1,6 @@
 set BUILD_DIR=..\..\search
 set EXE_FILE=mirusan_search.exe
-set TARGET_DIR=..\electron\dist
 
 copy build.spec %BUILD_DIR%
 cd %BUILD_DIR%
 pyinstaller build.spec
-copy dist\%EXE_FILE% %TARGET_DIR%
